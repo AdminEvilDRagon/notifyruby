72ae39abebfe567a6805724ccef578beb7d3125a
AdminEvilDRagon <92892215+AdminEvilDRagon@users.noreply.github.com>
2025-04-20 20:13:12 +0200

    notifyruby.lua

--- notifyruby.lua
+++ notifyruby.lua
@@ -16,8 +16,8 @@ Holder.Name = "NotifyHolder"
 Holder.Parent = ScreenGui
 
 local notifySound = Instance.new("Sound")
-notifySound.SoundId = "rbxassetid://639589431" -- Ersetze durch deine gewünschte Sound-ID
-notifySound.Volume = 7
+notifySound.SoundId = "rbxassetid://639589431" -- Sound-ID
+notifySound.Volume = 6
 notifySound.Parent = SoundService
 
 function NotifyLib:Notify(message, duration)
@@ -27,8 +27,8 @@ function NotifyLib:Notify(message, duration)
 	NotifyFrame.Size = UDim2.new(0, 0, 0, 60)
 	NotifyFrame.Position = UDim2.new(0.5, 0, 0, 0)
 	NotifyFrame.AnchorPoint = Vector2.new(0.5, 0)
-	NotifyFrame.BackgroundColor3 = Color3.fromRGB(40, 40, 40)  -- Weniger transparent, etwas dunkler
-	NotifyFrame.BackgroundTransparency = 0.15  -- Leicht durchscheinend
+	NotifyFrame.BackgroundColor3 = Color3.fromRGB(40, 40, 40)  
+	NotifyFrame.BackgroundTransparency = 0.15  
 	NotifyFrame.BorderSizePixel = 0
 	NotifyFrame.Parent = Holder
 
@@ -42,7 +42,7 @@ function NotifyLib:Notify(message, duration)
 	Title.TextSize = 16
 	Title.TextColor3 = Color3.fromRGB(255, 0, 143)  -- purpel text
 	Title.BackgroundTransparency = 1
-	Title.Position = UDim2.new(0.5, 0, 0, 5)  -- Titel oben mittig
+	Title.Position = UDim2.new(0.5, 0, 0, 5)  
 	Title.AnchorPoint = Vector2.new(0.5, 0)
 	Title.Size = UDim2.new(1, -20, 0, 18)
 	Title.TextXAlignment = Enum.TextXAlignment.Center
@@ -52,7 +52,7 @@ function NotifyLib:Notify(message, duration)
 	Msg.Text = message
 	Msg.Font = Enum.Font.GothamSemibold
 	Msg.TextSize = 18
-	Msg.TextColor3 = Color3.fromRGB(255, 255, 255)  -- Weißer Text
+	Msg.TextColor3 = Color3.fromRGB(255, 255, 255) 
 	Msg.BackgroundTransparency = 1
 	Msg.Position = UDim2.new(0, 10, 0, 25)
 	Msg.Size = UDim2.new(1, -20, 1, -30)
@@ -60,7 +60,7 @@ function NotifyLib:Notify(message, duration)
 	Msg.TextWrapped = true
 	Msg.Parent = NotifyFrame
 
-	-- Sound abspielen
+	
 	notifySound:Play()
 
 	local tweenIn = TweenService:Create(NotifyFrame, TweenInfo.new(0.3, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {
